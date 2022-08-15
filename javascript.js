f9ccdf4578a73e9e02f2cc0349ab1f4ccd441245
Kole Chaicharee <kole.chaicharee@gmail.com>
2022-08-15 16:42:11 +0900

    Add playRound() with playerChoice variable

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 // Create a function that randomly return either 'Rock' / 'Paper' / 'Scissors
 function getComputerChoice() {
     // Create a list of 'rock', 'paper', and 'scissors'
-    const choices = ["Rock", "Paper", "Scissors"];
+    const choices = ["rock", "paper", "scissors"];
     // Random and store the index with the length of the array
     const computerChoice = choices[Math.floor(Math.random() * choices.length)];
     // Return the result
@@ -9,3 +9,13 @@ function getComputerChoice() {
 }
 
 // Create a function that takes playerChoice and computerChoice as an input then return a string of result
+function playRound(playerSelection, computerSelection) {
+    // Create a variable to store player selection and ask for input
+    // Change the input value to lowercase for case-insensitive
+    playerChoice = prompt("Rock? Paper? or Scissors?").toLowerCase();
+    console.log(playerChoice);
+    // Create a varialbe to store computer selection that is returned from getComputerChoice function
+    // Compare the two selections, if true return the result message
+}
+
+playRound();
