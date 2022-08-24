629bf03ebb68ca5b827262d15dfbdb5e7f7ed6c2
Kole Chaicharee <kole.chaicharee@gmail.com>
2022-08-24 14:14:04 +0900

    Add main comments

--- javascript.js
+++ javascript.js
@@ -22,13 +22,15 @@ const rock = '<svg id="rock" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448
 const paper = '<svg id="paper" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M480 128v208c0 97.05-78.95 176-176 176h-37.72c-53.42 0-103.7-20.8-141.4-58.58l-113.1-113.1C3.906 332.5 0 322.2 0 312C0 290.7 17.15 272 40 272c10.23 0 20.47 3.906 28.28 11.72L128 343.4V64c0-17.67 14.33-32 32-32s32 14.33 32 32l.0729 176C192.1 248.8 199.2 256 208 256s16.07-7.164 16.07-16L224 32c0-17.67 14.33-32 32-32s32 14.33 32 32l.0484 208c0 8.836 7.111 16 15.95 16S320 248.8 320 240L320 64c0-17.67 14.33-32 32-32s32 14.33 32 32l.0729 176c0 8.836 7.091 16 15.93 16S416 248.8 416 240V128c0-17.67 14.33-32 32-32S480 110.3 480 128z"/></svg>';
 const scissors = '<svg id="scissors" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M512 192v111.1C512 383.4 447.4 448 368 448H288c-26.52 0-48-21.48-48-47.99c0-9.152 2.697-17.61 7.139-24.89C224.9 370.1 208 351.5 208 328c0-16.72 8.561-31.4 21.52-39.1H40c-22.09 0-40-17.9-40-39.99s17.91-39.1 40-39.1h229.5L60 142.2C42.93 136.8 31.99 121.1 31.99 104c0-3.973 .5967-8.014 1.851-12.01c5.35-17.07 21.08-28.04 38.06-28.04c4 0 8.071 .6085 12.09 1.889l279.2 87.22C364.8 153.6 366.4 153.8 368 153.8c6.812 0 13.12-4.375 15.27-11.23c.4978-1.588 .7346-3.195 .7346-4.777c0-6.807-4.388-13.12-11.23-15.25l-72.54-22.67l14.29-17.85C323.6 70.67 337.4 64.04 352 64.04h48c10.39 0 20.48 3.359 28.8 9.592l38.41 28.79c25.2 18.91 40.53 47.97 43.55 79.04C511.5 184.9 512 188.4 512 192z"/></svg>';
 
-// Randomly get
+// Randomly get computer choice from choices list for comparing with user choice
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
     const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+
     return computerChoice;
 }
 
+// When the either side reaches 5 wins, this function is called in playRound() function to annouce the result to the user
 function annouceResult(playerScore, computerScore, result) {
     if (computerScore === 5) {
         result.textContent = "Sorry. You LOSE! :P";
@@ -38,11 +40,13 @@ function annouceResult(playerScore, computerScore, result) {
     }
 }
 
+// This function will be called to annouce the round result when the choices are compared in playRound() function
 function displayResult(result){
     roundResult.innerHTML = result;
 
     const svgs = document = roundResult.querySelectorAll('svg')
     let svgStyle = "width: 20px; height: 20px; "
+
     svgs.forEach(svg => {
         svg.style.cssText = svgStyle;
     });
@@ -51,20 +55,22 @@ function displayResult(result){
     actionHistory.appendChild(roundResult);
 }
 
+// This function will be called to reset the game to its original state when the user click "Play again?"
 function resetGame() {
     computerScore = 0;
     userScore = 0;
+    mputerCount.textContent = 0;
+    userCount.textContent = 0;
     result.remove();
     restartButton.remove();
     actionHistory.remove();
     roundResult.remove();
     action.remove();
-    computerCount.textContent = 0;
-    userCount.textContent = 0;
     choices.forEach(choice => userChoice.appendChild(choice));
     container.insertBefore(question, userChoice);
 }
 
+// This function is called when the playRound() function is called to display the choice of the user and the computer for clarity
 function displayAction(userChoice, comChoice) {
     let svgUser = "";
     let svgCom = "";
@@ -91,13 +97,15 @@ function displayAction(userChoice, comChoice) {
     }
 
     action.innerHTML = "<div>" + svgUser + "</div>" + "<div>" + svgCom + "</div>";
+
     score.appendChild(action);
 }
 
+// Global variable to increment scores in each round (for playRound() function)
 let userScore = 0;
 let computerScore = 0;
 
-// Create a function that takes playerChoice and computerChoice as an input then return a string of result
+// (Main) This function will be called when the user clicked one of the choice
 function playRound(e) {
     const playerSelection = e.target.id;
     const computerSelection = getComputerChoice();
@@ -129,6 +137,7 @@ function playRound(e) {
 
     if (computerScore === 5 || userScore === 5) {
         container.insertBefore(result, container.firstChild);
+
         annouceResult(userScore, computerScore, result);
 
         choices.forEach(choice => choice.remove());
@@ -140,6 +149,9 @@ function playRound(e) {
     }
 }
 
+// Selected nodes of rock, paper, and scissors button
 const userChoice = document.querySelector('.user-choice');
 const choices = userChoice.querySelectorAll('button');
+
+// When one of the choices are clicked, call playRound() function to play a round
 choices.forEach(choice => choice.addEventListener('click', playRound));
